c3b54f2d46a41de024a740e01a125d968aa99ec9
saurabh.s.ed <saurabh.singh@edvanta.com>
2024-05-11 18:03:25 +0530

    add some files

---
+++ src/Util/ServerPath.js
@@ -0,0 +1,8 @@
+export const baseURL = "http://localhost:3300/";
+export const devKey = "jaZfYL80ZkXc2mmUfVv6BzcuZsNXK5dm";
+
+//aws credentials
+export const Bucket_name = "your_Bucket_name";
+export const aws_access_key_id = "your_access_key";
+export const aws_secret_access_key = "your_secret_key";
+export const space_End_point = "space_End_point";
